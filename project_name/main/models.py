847d172b1c9592ffc33dfac8e5eec0073efc1550
Ilya <kaduk9393@gmail.com>
2019-10-15 07:35:19 +0300

    Add model and form for files-images

--- project_name/main/models.py
+++ project_name/main/models.py
@@ -2,20 +2,8 @@ from django.db import models
 
 
 class Article(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    type = models.CharField(max_length=30)
-    status = models.CharField(max_length=20)
-
-
-class Display(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    page = models.CharField(max_length=100)
-
-
-class Images(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='article')
+    price = models.FileField(upload_to='price')
 
 
 
